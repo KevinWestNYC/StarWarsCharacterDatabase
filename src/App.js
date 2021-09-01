8ce4cb29e9aba33e40805ee8e7f68ac561588ae4
Kevin W <kevinwestnyc@gmail.com>
2021-09-01 15:25:39 -0400

    got species and homeworld to display properly

--- src/App.js
+++ src/App.js
@@ -15,42 +15,60 @@ function StarWarsApp() {
   useEffect(() => {
     setLoading(true)
     const getCharacterData = async () => {
-      await axios.get(currentPageUrl).then(response => {
+      await axios.get(currentPageUrl).then(async response => {
         setLoading(false)
-        
-        // setNextPageUrl(response.data.next)
-        // setPrevPageUrl(response.data.previous)
         const characterData = response.data.results
         
         for(const character of characterData){
           const homeworld = character.homeworld;
-          const homeworldResponse = axios.get(homeworld).then((res) => 
-          res);
-          character.homeworld = homeworldResponse.name;
-          console.log("homeworlds:" + homeworld)
+          const homeworldResponse = await axios.get(homeworld)
+          character.homeworld = homeworldResponse.data.name;
+          
         }
+
+        for(const character of characterData){
+          const species = character.species;
+          const speciesResponse = await axios.get(species)
+          
+          character.species = speciesResponse.data.name
+        }  
         
-        setCharacters(characterData)
-        
+        setCharacters(characterData)        
     });
-  };
-  
+  };  
       getCharacterData();
     }, [currentPageUrl])
 
     console.log("characters: ", characters)
 
+    useEffect(() => {
+      setLoading(true)
+      const fetchCharacters = async () => {
+      const result = await axios.get(`https://swapi.dev/api/people/?search=${query}`)
+
+        setCharacters(result.data)
+        setLoading(false)
+      }
+
+      fetchCharacters()
+    }, [currentPageUrl])
+
     // useEffect(() => {
     //   setLoading(true)
-    //   const fetchCharacters = async () => {
-    //     const result = await axios(`https://swapi.dev/api/people/?search=${query}`)
-
-    //     setCharacters(result.data)
-    //     setLoading(false)
-    //   }
-
-    //   fetchCharacters()
-    // }, [query])
+    //   const getCharacterData = async () => {
+    //     await axios.get(currentPageUrl).then(async response => {
+    //       setLoading(false)
+    //       const characterData = response.data.results
+          
+    //     setNextPageUrl(response.data.next)
+    //     setPrevPageUrl(response.data.previous)
+          
+    //       setCharacters(characterData)
+          
+    //     });
+    //   };      
+    //       getCharacterData();
+    //     }, [currentPageUrl])
 
     function goToNextPage() {
       setCurrentPageUrl(nextPageUrl)
@@ -61,44 +79,45 @@ function StarWarsApp() {
     }
   
   
-  if(loading) return "Loading..."
+    if(loading) return <div className="text-center"><h1>Loading...</h1></div>
 
-  return (
-    <div className="App">
-      <h1>Star Wars Character Search</h1>
-      <div className="container text-center">
-        <Search getQuery={(query) => setQuery(query)} />
-        <table className="table table-bordered table-hover">
-            <thead>
-              <th>Name</th>
-              <th>Birth Date</th>
-              <th>Height</th>
-              <th>Mass</th>
-              <th>Homeworld</th>
-              <th>Species</th>
-            </thead>
-            <tbody>
-          {characters.length > 0 ?
-          characters.map((character) => (
-            <tr key={character.name} >
-              <td>{character.name}</td>
-              <td>{character.birth_year}</td>
-              <td>{character.height}</td>
-              <td>{character.mass}</td>
-              <td>{character.homeworld}</td>
-              <td>{character.species}</td>
-            </tr>
-          ))
-          : null}
-        </tbody>
-          </table>
-          <Pagination 
-            goToNextPage={nextPageUrl ? goToNextPage : null}
-            goToPrevPage={prevPageUrl ? goToPrevPage : null}
-          />
-          </div>      
-    </div> 
-  );
-}
-
-export default StarWarsApp;
+    return (
+      <div className="App">
+        <h1 className="text-center">Star Wars Character Search</h1>
+        <div className="container text-center">
+          <Search getQuery={(query) => setQuery(query)} />
+          <table className="table table-bordered table-hover">
+              <thead>
+                <th>Name</th>
+                <th>Birth Date</th>
+                <th>Height</th>
+                <th>Mass</th>
+                <th>Homeworld</th>
+                <th>Species</th>
+              </thead>
+              <tbody>
+            {characters.length > 0 ?
+            characters.map((character) => (
+              <tr key={character.name} >
+                <td>{character.name}</td>
+                <td>{character.birth_year}</td>
+                <td>{character.height}</td>
+                <td>{character.mass}</td>
+                <td>{character.homeworld}</td>
+                <td>{character.species || "Humanoid"}</td>
+              </tr>
+            ))
+            : null}
+          </tbody>
+            </table>
+            <Pagination 
+              goToNextPage={nextPageUrl ? goToNextPage : null}
+              goToPrevPage={prevPageUrl ? goToPrevPage : null}
+            />
+            </div>      
+      </div> 
+    );
+  }
+  
+  export default StarWarsApp;
+  
